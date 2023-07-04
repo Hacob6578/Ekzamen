3bd00d0ba0295c87692212d0fa036f69d14f4301
316de <316de@315-DE-9>
2023-07-04 09:29:38 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Garden
+    {
+        public string Adress { get; set; }
+        public string Owner { get; set; }
+        public string Flowers { get; set; }
+        public List<Flower> Library = new List<Flower>();
+        public Garden(string add, string own, string flw)
+        {
+            Adress = add;
+            Owner = own;
+            Flowers = flw;
+        }
+        
+        public void ShowAll()
+        {
+            Console.WriteLine("Сад:\n" + $"\tАдресс: {Adress}\n" + $"\tВладелец: {Owner}\n" + $"\tЦветок: {Flowers}\n");
+            Library.Sort();
+            foreach (Flower flower in Library)
+            {
+                flower.Show();
+            }
+        }
+    }
+
+
+    public class Flower : IComparable
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Flower(string t, string des)
+        {
+            Title = t;
+            Description = des;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Цветок:\n" + $"\tНазвание: {Title}\n" + $"\tОписание: {Description}");
+        }
+        public int CompareTo(object obj)
+        {
+            return (this.Description + this.Title).CompareTo(Description + Title);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Flower flower = new Flower("Роза", "Нежный и ароматный цветок, который бывает различных цветов, идеальный подарок для девушки");
+            Flower flower2 = new Flower("Тюльпан", "Тоже хороший цветов, который подойдёт для любой дамы");
+            Flower flower3 = new Flower("Гипсофила", "Небольшой набор этого цветка удивит любого человека");
+            Flower flower4 = new Flower("Жасмин", "Цветок");
+            Garden garden = new Garden("Улица Долгорукого 3", "Иван", "Роза");
+            Garden garden2 = new Garden("Менжинская 15", "Анастасия", "Тюльпан");
+            Garden garden3 = new Garden("Летчика Бабушкина 3", "Вадим", "Гипсофила");
+            Garden garden4 = new Garden("Изумрудная 24", "Жасмина", "Жасмин");
+            flower.Show();
+            flower2.Show();
+            flower3.Show();
+            flower4.Show();
+            garden.ShowAll();
+            garden2.ShowAll();
+            garden3.ShowAll();
+            garden4.ShowAll();
+            Console.ReadKey();            
+        }
+    }
+}
