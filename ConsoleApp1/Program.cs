3a482ef665bcf2b602d9afbb7e7e3b9c48d4ef02
FetkulinVV <316de@315-DE-9>
2023-07-04 09:38:40 +0300

    Коментарии ещё

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,18 +15,22 @@ namespace ConsoleApp1
             Flower flower2 = new Flower("Тюльпан", "Тоже хороший цветов, который подойдёт для любой дамы");
             Flower flower3 = new Flower("Гипсофила", "Небольшой набор этого цветка удивит любого человека");
             Flower flower4 = new Flower("Жасмин", "Цветок");
+            //Добавил Класс на вывод цветок
             Garden garden = new Garden("Улица Долгорукого 3", "Иван", "Роза");
             Garden garden2 = new Garden("Менжинская 15", "Анастасия", "Тюльпан");
             Garden garden3 = new Garden("Летчика Бабушкина 3", "Вадим", "Гипсофила");
             Garden garden4 = new Garden("Изумрудная 24", "Жасмина", "Жасмин");
+            //Добавил Класс на вывод Сад
             flower.Show();
             flower2.Show();
             flower3.Show();
             flower4.Show();
+            //Добавил Класса 
             garden.ShowAll();
             garden2.ShowAll();
             garden3.ShowAll();
             garden4.ShowAll();
+            //Добавил Класса
             Console.ReadKey();
             //тут в общем показан вывод всего написанного
         }
